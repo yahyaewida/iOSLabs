b9224d03131857628cbe10e6ed13ea73e815dbbe
Yahya Ewida <yahyaewida@yahoo.com>
2020-03-29 07:40:08 -0700

    Day 4 swift added

---
+++ Swift/Day4/CollectionViewDemo/CollectionViewDemo/CollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  CollectionViewCell.swift
+//  CollectionViewDemo
+//
+//  Created by Esraa Hassan on 3/29/20.
+//  Copyright © 2020 Yahya. All rights reserved.
+//
+
+import UIKit
+
+
+class CollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cellImageView: UIImageView!
+    
+}
---
+++ Swift/Day4/CollectionViewDemo/build/Pods.build/Debug-iphoneos/Pods-CollectionViewDemo.build/DerivedSources/Pods_CollectionViewDemo_vers.c
@@ -0,0 +1,5 @@
+ extern const unsigned char Pods_CollectionViewDemoVersionString[];
+ extern const double Pods_CollectionViewDemoVersionNumber;
+
+ const unsigned char Pods_CollectionViewDemoVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Pods_CollectionViewDemo  PROJECT:Pods-1" "\n";
+ const double Pods_CollectionViewDemoVersionNumber __attribute__ ((used)) = (double)1.;
